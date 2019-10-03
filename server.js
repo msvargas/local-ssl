244520161bde553bd83bd7e78d631af3ff3aaa32
Michael Vargas <msvargas97@gmail.com>
2019-10-03 07:04:43 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// Setting up local https support
+var fs = require('fs');
+var https = require('https');
+const PORT = 3000;
+
+var options = {
+    key: fs.readFileSync('../vcs.key'),
+    cert: fs.readFileSync('../vcs.crt'),
+    passphrase: '901089033',
+    requestCert: false,
+    rejectUnauthorized: false
+};
+
+function handleRequest( request, response ){
+    response.end( 'It Works!' );
+}
+
+
+// Start server
+var server = https.createServer(options,handleRequest)
+
+server.listen( PORT, "vcs.local", function(){
+    console.log( "Server listening on %s", `https://vcs.local.com:${PORT}` );
+});
